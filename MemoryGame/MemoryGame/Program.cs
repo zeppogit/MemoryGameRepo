46b2b3cfa2405a0641efdea942fae84126e9a30f
zeppogit <zeppocode@gmail.com>
2022-03-22 14:57:24 -0400

    to add first commit with MemoryGame solution and project folder, with Program.cs.  Base class to be added next

---
+++ MemoryGame/MemoryGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using CodeLouisvilleLibrary;
+
+namespace CSharpProjectApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //THIS WILL CONTAIN THE RUN() INSTRUCTIONS FOR THE PROGRAM, NOT THE USER INTERFACE
+
+
+            // the following temporary code used only to verify dependency is working
+            /*         bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Does this make sense? ");
+
+                      if (doesThisMakeSense)
+                          Console.WriteLine("I've got this!!!");
+                      else
+                          Console.WriteLine("Blame John");   
+            */
+            // use Run() from base class
+
+
+        }
+    }
+}
+
+
